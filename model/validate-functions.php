af7a9a3e188d14c5468632dce43f58457a8d95ca
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-01-31 13:43:43 -0500

    added model directory

---
+++ model/validate-functions.php
@@ -0,0 +1,17 @@
+<?php
+
+/* validate a color
+ * @param String color
+ * @return boolean
+ */
+function validColor($color)
+{
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
+function validString($string)
+{
+    global $f3;
+    return cytype_alpha($string);
+
+}
